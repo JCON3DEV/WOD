6185c16fe7f51863c7cabf1cf77fcafecb0f0a5d
Jordan <jconheasman@gmail.com>
2020-11-27 21:05:22 -0800

    minor changes to app.js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
 import React from 'react';
 import logo from './mintbean.png';
+// import Button from "./compone  nts/button";
+import Counter from "./components/counter";
 import './styles/index.css';
 
 function App() {
   return (
     <div className="App">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          <code>App.js</code> is the entrypoint to your app.
-        </p>
-        <h2>HAPPY HACKING!</h2>
-        <h3>This is the WOD generator</h3>
+      <img src={logo} className="App-logo" alt="logo" />
+      <p>
+        <code>App.js</code> is the entrypoint to your app.
+      </p>
+      <h2>HAPPY HACKING!</h2>
+      <h3>This is the WOD generator</h3>
 
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <input type="text" />    
+      <button>Add Exercise</button>
+      <button>Clear Exercise</button>
+      <Counter />
     </div>
   );
 }
